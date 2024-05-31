ef08dec5a6619d94b527164769d94b95af6a7d00
soandrade6 <sofia.andradep06@gmail.com>
2024-05-30 22:26:07 -0500

    Product model created

---
+++ server/src/models/Product.js
@@ -0,0 +1,32 @@
+import sequelize from "../database/database.js";
+import { DataTypes } from "sequelize";
+
+export const Product = sequelize.define(
+  "products",
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    description: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    price: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+  },
+  {
+    timestamps: false,
+  }
+);
