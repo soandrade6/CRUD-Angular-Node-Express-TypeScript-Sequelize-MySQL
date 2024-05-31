16ffc9565864ea1326a2bae1f0ebf25823e1b6b3
soandrade6 <sofia.andradep06@gmail.com>
2024-05-30 23:00:42 -0500

    routes fixed

--- server/src/routes/products.routes.js
+++ server/src/routes/products.routes.js
@@ -10,7 +10,7 @@ const router = Router();
 
 router.get("/products", getProducts);
 router.post("/products", createProduct);
-router.put("/products", updateProduct);
-router.delete("/products", deleteProduct);
+router.put("/products/:id", updateProduct);
+router.delete("/products/:id", deleteProduct);
 
 export default router;
