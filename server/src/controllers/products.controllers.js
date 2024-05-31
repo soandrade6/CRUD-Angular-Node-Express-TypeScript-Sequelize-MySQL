729569e9a6c89e041d9d4b574bfdbc665df1caeb
soandrade6 <sofia.andradep06@gmail.com>
2024-05-30 22:35:52 -0500

    Product controllers created

---
+++ server/src/controllers/products.controllers.js
@@ -0,0 +1,59 @@
+import Product from "../models/Product.js";
+
+export const getProducts = async (req, res) => {
+  try {
+    const products = await Product.findAll();
+    res.json(products);
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+};
+
+export const createProduct = async (req, res) => {
+  try {
+    const { name, description, price, stock } = req.body;
+    const newProduct = await Product.create({
+      name,
+      description,
+      price,
+      stock,
+    });
+    res.json(newProduct);
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+};
+
+export const updateProduct = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { name, description, price, stock } = req.body;
+
+    const product = await Product.findByPk(id);
+
+    product.name = name;
+    product.description = description;
+    product.price = price;
+    product.stock = stock;
+    await product.save();
+
+    res.json(product);
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+};
+
+export const deleteProduct = async (req, res) => {
+  try {
+    const { id } = req.params;
+    await Product.destroy({
+      where: {
+        id,
+      },
+    });
+
+    res.sendStatus(204);
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+};
