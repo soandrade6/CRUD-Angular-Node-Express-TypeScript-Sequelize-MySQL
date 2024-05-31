d55b33bf90eb7423c8b40faa5321282535b887c0
soandrade6 <sofia.andradep06@gmail.com>
2024-05-30 22:14:50 -0500

    sql script created

---
+++ server/scrip.sql
@@ -0,0 +1,23 @@
+
+CREATE SCHEMA IF NOT EXISTS product_manager;
+
+USE product_manager;
+
+CREATE TABLE IF NOT EXISTS products (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    price INT NOT NULL,
+    stock INT NOT NULL,
+);
+
+INSERT INTO products (name, price, stock) VALUES
+    ('Laptop', 1200, 10),
+    ('Smartphone', 800, 25),
+    ('Tablet', 600, 15),
+    ('Headphones', 150, 50),
+    ('Smartwatch', 200, 30),
+    ('Monitor', 300, 20),
+    ('Keyboard', 100, 40),
+    ('Mouse', 50, 60),
+    ('Printer', 250, 10),
+    ('External Hard Drive', 120, 35);
