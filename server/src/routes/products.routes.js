33314664a9963caceb54fda6c359087ac25d075b
soandrade6 <sofia.andradep06@gmail.com>
2024-05-30 22:39:56 -0500

    Products routes created

---
+++ server/src/routes/products.routes.js
@@ -0,0 +1,14 @@
+import Router from "express";
+import {
+  createProduct,
+  deleteProduct,
+  getProducts,
+  updateProduct,
+} from "../controllers/products.controllers.js";
+
+const router = Router();
+
+router.get("/products", getProducts);
+router.post("/products", createProduct);
+router.put("/products", updateProduct);
+router.delete("/products", deleteProduct);
