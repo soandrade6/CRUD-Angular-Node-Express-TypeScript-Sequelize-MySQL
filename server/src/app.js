46ce861703bf648fa286c4a6b639e817313593bf
soandrade6 <sofia.andradep06@gmail.com>
2024-06-01 18:18:27 -0500

    cors configuration

--- server/src/app.js
+++ server/src/app.js
@@ -1,11 +1,14 @@
 import express from "express";
 import productsRoutes from "./routes/products.routes.js";
 
+const cors = require("cors");
+
 const app = express();
 
 // middlewares
 app.use(express.json());
 
 app.use(productsRoutes);
+app.use(cors());
 
 export default app;
